ce5fca9895605c23e651ee1e41f8861d52c48f0f
modusami <128193218+modusami@users.noreply.github.com>
2024-10-19 05:15:27 -0400

    Update homeScreen

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -5,8 +5,42 @@
 
 const tintColorLight = "#0a7ea4";
 const tintColorDark = "#fff";
-const redTheme = "#d52600";
-const whiteTheme = "#ffffff";
+
+export const Colors2 = {
+	// Red shades
+	lightRed: "#FF6666",
+	mediumRed: "#FF0000",
+	darkRed: "#990000",
+
+	// Blue shades
+	lightBlue: "#66CCFF",
+	mediumBlue: "#0066CC",
+	darkBlue: "#003366",
+
+	// Green shades
+	lightGreen: "#99FF99",
+	mediumGreen: "#33CC33",
+	darkGreen: "#006600",
+};
+
+// You can also create separate objects for each color family if preferred
+export const MariottShades = {
+	light: Colors2.lightRed,
+	medium: Colors2.mediumRed,
+	dark: Colors2.darkRed,
+};
+
+export const BlueShades = {
+	light: Colors2.lightBlue,
+	medium: Colors2.mediumBlue,
+	dark: Colors2.darkBlue,
+};
+
+export const GreenShades = {
+	light: Colors2.lightGreen,
+	medium: Colors2.mediumGreen,
+	dark: Colors2.darkGreen,
+};
 
 export const Colors = {
 	light: {
