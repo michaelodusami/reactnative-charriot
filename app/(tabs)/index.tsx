2cd2faad7ee6e302ba12b02fd984d33d4678cf0b
modusami <128193218+modusami@users.noreply.github.com>
2024-10-19 04:38:07 -0400

    Getting Rid Of NativeWInd, Reverting

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,12 @@
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import { Text } from "react-native";
 const HomePage = () => {
 	return (
 		<SafeAreaView>
 			<ThemedView>
-				<ThemedText>Welcome, Mike</ThemedText>
+				<Text className="bg-red-500 text-white">Welcome, Mike</Text>
 			</ThemedView>
 		</SafeAreaView>
 	);
