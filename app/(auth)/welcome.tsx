e6a19b07cbcb82212df24f3ffa510b0dbd985f42
modusami <128193218+modusami@users.noreply.github.com>
2024-10-19 23:06:16 -0400

    Update

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,13 @@
+import { ThemedText } from "@/components/ThemedText";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Onboarding = () => {
+	return (
+		<SafeAreaView>
+			<ThemedText>Welcome</ThemedText>
+		</SafeAreaView>
+	);
+};
+
+export default Onboarding;
