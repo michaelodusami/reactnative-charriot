f6f85b586985135dc0350bd4947ce21f24004fda
modusami <128193218+modusami@users.noreply.github.com>
2024-10-19 23:16:27 -0400

    install natvueinwd

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,12 +1,13 @@
 import { ThemedText } from "@/components/ThemedText";
-import { Text } from "react-native";
+import { StatusBar, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Onboarding = () => {
 	return (
-		<SafeAreaView>
-			<ThemedText>Welcome</ThemedText>
-		</SafeAreaView>
+		<View className="flex-1 items-center justify-center bg-white">
+			<Text>Open up App.js to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
 	);
 };
 
